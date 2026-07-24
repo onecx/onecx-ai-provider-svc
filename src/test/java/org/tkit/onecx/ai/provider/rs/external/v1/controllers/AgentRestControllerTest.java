25e9974d19dbfab46e33bdd3d751c2791a24a010
fadeleus <fadel.kaadan@gmail.com>
2026-07-24 14:41:17 +0200

    feat: test agent rest controller

---
+++ src/test/java/org/tkit/onecx/ai/provider/rs/external/v1/controllers/AgentRestControllerTest.java
@@ -0,0 +1,54 @@
+package org.tkit.onecx.ai.provider.rs.external.v1.controllers;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.ai.provider.test.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
+import org.tkit.quarkus.test.WithDBData;
+
+import gen.org.tkit.onecx.ai.provider.rs.external.v1.model.*;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@TestHTTPEndpoint(AgentRestController.class)
+@WithDBData(value = "data/testdata-internal.xml", deleteBeforeInsert = true, deleteAfterTest = true, rinseAndRepeat = true)
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ai:all", "ocx-ai:read", "ocx-ai:write", "ocx-ai:delete" })
+class AgentRestControllerTest extends AbstractTest {
+
+    @Test
+    void findAgentBySearchCriteriaTest() {
+        var criteria = new AgentSearchCriteriaDTOV1();
+        var data = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .body(criteria)
+                .post("/search")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .extract()
+                .as(AgentPageResultDTOV1.class);
+
+        assertThat(data).isNotNull();
+        assertThat(data.getTotalElements()).isEqualTo(4);
+        assertThat(data.getStream()).isNotNull().hasSize(4);
+
+        criteria.setPageNumber(1);
+        criteria.setPageSize(10);
+        data = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .body(criteria)
+                .post("/search")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .extract()
+                .as(AgentPageResultDTOV1.class);
+
+        assertThat(data.getTotalElements()).isEqualTo(1);
+    }
+}
